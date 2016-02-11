89e646b9b3e4ba5a1baf1316fd7514c301f107ec
inetFight <d-litvinov@ukr.net>
2016-02-11 14:43:00 +0200

    q

--- restprj/src/main/java/com/rest/test/Deposit.java
+++ restprj/src/main/java/com/rest/test/Deposit.java
@@ -12,72 +12,61 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-
-
 @Path("/deposit")
 public class Deposit {
- 
 
+	@GET
+	@Path("/{username}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String get(@PathParam("username") String username) {
 
+		for (int i = 0; i < User.u.length; i++) {
+			if (User.u[i] != null)
+				if (User.u[i].getName().equals(username) == true) {
+					return "User " + User.u[i].getName() + " has " + User.u[i].getDeposit() + " on deposit";
 
+				}
 
-
-@GET
-@Path("/{username}")
-@Produces(MediaType.TEXT_PLAIN)
-public String get(@PathParam("username") String username){
-	
-	for (int i = 0; i <User.u.length; i++) {
-		if(User.u[i] != null)
-		if(User.u[i].getName().equals(username) == true){
-			 return "User " + User.u[i].getName() + " has " + User.u[i].getDeposit() + " on deposit";
-			 
 		}
-		
+
+		return "User not found";
 	}
 
-	
-	
-
-	return "User not found";
-}
-
-	
-	
-@PUT
-@Path("/{username}/{dep}")
-@Produces(MediaType.TEXT_PLAIN)
-public String add(@PathParam("username") String username, @PathParam("dep") int dep){
+	@PUT
+	@Path("/{username}/{dep}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String add(@PathParam("username") String username, @PathParam("dep") int dep) {
 		for (int i = 0; i < User.u.length; i++) {
-			if(User.u[i] != null){
-				if(User.u[i].getName().equals(username) == true){
+			if (User.u[i] != null) {
+				if (User.u[i].getName().equals(username) == true) {
 					User.u[i].setDeposit(dep);
-					 return " deposit " + User.u[i].getName() + " increased on " + dep + " . Deposit = " + User.u[i].getDeposit();
-				
-				}
-			}}
-			for (int j = 0; j < User.u.length; j++) {
-				if(User.u[j] == null){
-					User.u[j] = new User(username, dep);
-					return "User not found. Created new user with name " + username + " and deposited " + dep;
+					return " deposit " + User.u[i].getName() + " increased on " + dep + " . Deposit = "
+							+ User.u[i].getDeposit();
+
 				}
 			}
-		
+		}
+		for (int j = 0; j < User.u.length; j++) {
+			if (User.u[j] == null) {
+				User.u[j] = new User(username, dep);
+				return "User not found. Created new user with name " + username + " and deposited " + dep;
+			}
+		}
+
 		return "Array is max! Error";
 	}
-	
-@GET
-@Path("/")
-@Produces(MediaType.TEXT_PLAIN)
-public String Deposit(){
+
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String Deposit() {
 		String s = " ";
 		for (int i = 0; i < User.u.length; i++) {
-			if(User.u[i] != null){
+			if (User.u[i] != null) {
 				s += User.u[i].getName() + " = " + User.u[i].getDeposit() + "\n";
 			}
 		}
-	return s;
+		return s;
 	}
-	
+
 }
-	
